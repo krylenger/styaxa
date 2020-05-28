4a5993d0eef27078f3e7ed64a540866b42abf629
krylenger <krylenger@gmail.com>
2020-05-28 12:25:00 +0800

    improving style

--- src/components/ContactInfo/ContactInfo.js
+++ src/components/ContactInfo/ContactInfo.js
@@ -1,20 +1,25 @@
 import React from 'react';
 import './ContactInfo.css';
+import logoInstagram from '../../pics/logos/0817158f3a05c0d62de647c28f4cde54.jpg';
 
 const ContactInfo = () => {
     return(
         <div className='contact-info-container'>
             <div className='contact-info-container-inner'>
-                <div className='header'>Contact Me!</div>
+                <div className='header-contact'>Contact Me!</div>
                 <div className='IPM'>
-                    <div className='instagram'>
-                        <div className='ig'>Instagram: </div>
-                        <div><a href='https://www.instagram.com/styaxa/'>@styaxa</a></div>
-                    </div>
-                    <div >
+                    <div className='IPM-section'>
+                    <div className='logo phone'></div>
                         <div>+62010101010</div>
                     </div>
-                    <div>
+                    <div className='IPM-section instagram'>
+                        <div className='logo'><a href='https://www.instagram.com/styaxa/'>@styaxa</a></div>
+                        <div className='ig'> 
+                            <a href='https://www.instagram.com/styaxa/'>@styaxa</a> 
+                        </div>
+                    </div>
+                    <div className='IPM-section'>
+                    <div className='logo mail'></div>
                         <div className='email'>example@gmail.com</div>
                     </div>
                 </div>
